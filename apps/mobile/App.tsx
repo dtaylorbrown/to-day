b014856ae9dbf976a8461dc23edd9c1a268e4436
dtaylorbrown <daniella.taylor92@gmail.com>
2024-06-10 10:07:39 +0100

    :bug: fix mobile crashing

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
-    <View>
+    <View style={styles.container}>
       <StatusBar style="auto" />
     </View>
   );
